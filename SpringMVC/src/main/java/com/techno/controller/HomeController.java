098016c19e247c4a149d968faa58225a02eb2be1
a <a@DESKTOP-0T7T504>
2021-02-25 23:26:56 +0530

    session 2

--- SpringMVC/src/main/java/com/techno/controller/HomeController.java
+++ SpringMVC/src/main/java/com/techno/controller/HomeController.java
@@ -1,15 +1,39 @@
 package com.techno.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController 
 {
+	/*
 	@RequestMapping("/home")
-	public String home() 
+	public String home(Model model) 
 	{
+		model.addAttribute("data","i am sending a data");
 		return "home";
+	}
+	*/
+/*
+	@RequestMapping("/home")
+	public ModelAndView home() 
+	{
+		ModelAndView mv= new ModelAndView();
+		mv.addObject("data","i am sending a data");
+		mv.setViewName("home");
+		return mv;
+	}
+*/	
+	@RequestMapping(value="/home", method = RequestMethod.POST)
+	public ModelAndView home() 
+	{
+		ModelAndView mv= new ModelAndView();
+		mv.addObject("data","i am sending a data");
+		mv.setViewName("home");
+		return mv;
 	}
 
 }
--- SpringMVC/src/main/webapp/WEB-INF/views/home.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1" isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +8,8 @@
 </head>
 <body>
 <h2>home page called</h2>
+<h3><%=request.getAttribute("data")%></h3>
+
+<h3>${data}</h3>
 </body>
 </html>
